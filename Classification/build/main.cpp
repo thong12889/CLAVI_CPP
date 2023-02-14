c541a35ad4ec4f94170a549cd261c82ba6abf88a
Peace-Sri <peacesrihera@gmail.com>
2023-02-14 16:59:58 +0900

    Delete non necessary Operation

--- Classification/build/main.cpp
+++ Classification/build/main.cpp
@@ -39,21 +39,6 @@ T vectorProduct(const std::vector<T>& v)
 	return accumulate(v.begin(), v.end(), 1, std::multiplies<T>());
 }
 
-template <typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
-{
-	os << "[";
-	for(int i=0; i < v.size(); ++i)
-	{
-		os << v[i];
-		if(i != v.size() - 1)
-		{
-			os << ", ";
-		}
-	}
-	os << "]";
-	return os;
-}
 
 static void get_results_pixel(const int64_t* pred, std::vector<int64_t> pred_dim, std::vector<cv::Vec3b> color, cv::Mat& mat)
 {	
@@ -92,66 +77,6 @@ static void decode_outputs(const int64_t* pred, std::vector<int64_t> pred_dim, c
 	cv::resize(result_img, result_img, cv::Size(img_w, img_h));
 }
 
-std::ostream& operator<<(std::ostream& os, const ONNXTensorElementDataType& type)
-{
-	switch(type)
-	{
-		case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UNDEFINED:
-			os << "undefined";
-			break;
-		case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT:
-			os << "float";
-			break;
-		case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT8:
-			os << "uint8_t";
-			break;
-		case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT8:
-			os << "int8_t";
-			break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT16:
-            		os << "uint16_t";
-           	 	break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT16:
-            		os << "int16_t";
-            		break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32:
-            		os << "int32_t";
-            		break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64:
-            		os << "int64_t";
-            		break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING:
-            		os << "std::string";
-            		break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL:
-            		os << "bool";
-            		break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16:
-            		os << "float16";
-            		break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE:
-            		os << "double";
-            		break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT32:
-            		os << "uint32_t";
-            		break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT64:
-            		os << "uint64_t";
-            		break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX64:
-            		os << "float real + float imaginary";
-            		break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX128:
-            		os << "double real + float imaginary";
-            		break;
-        	case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BFLOAT16:
-            		os << "bfloat16";
-            		break;
-        	default:
-            		break;
-	}
-	return os;
-}
 
 std::vector<std::string> readLabels(std::string& labelFilepath)
 {
